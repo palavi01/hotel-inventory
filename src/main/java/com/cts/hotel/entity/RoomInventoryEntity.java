088de0ad78eb7fc1d3bc960a32973f947f800505
avi3s <avirup.pal@gmail.com>
2021-12-29 22:24:06 +0530

    Update RoomInventoryEntity.java

--- src/main/java/com/cts/hotel/entity/RoomInventoryEntity.java
+++ src/main/java/com/cts/hotel/entity/RoomInventoryEntity.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "room")
+@Document(collection = "room_inventory")
 public class RoomInventoryEntity {
 
 	@Id 
